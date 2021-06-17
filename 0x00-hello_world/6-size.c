a3d6088d50949a1b169ea3242dc7ab7053fdaf84
Gbani-10 <gbani160@gmail.com>
2021-06-17 13:57:47 -0700

    size ot computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,21 @@
-#include <stdio.h>
+ #include <stdio.h>
 
  /**
- * main: print the string in the put function
- *Description: using the main function
- * this program prints "the size of the various type of competer sizes
- *return: 0
+ * main - entry point
+ *
+ *Return: Always 0 (Success)
  */
-
  int main(void)
  {
- char c;
- int i;
- long 1i;
- long long 11i;
- float f;
-
- printf("Size of a char: 1%d byte(s)\n", sizeof(c));
- printf("Size of an int: 1%d byte(s)\n", sizeof(i));
- printf("Size of a long int: 1%d byte(s)\n", sizeof(1i));
- printf("Size of a long long int: 1%d byte(s)\n", sizeof(11i));
- printf("size of a float: 1%d byte(s)\n", sizeof(f));
-
+ char charType;
+ int intType;
+ long 1intType;
+ long long LinType;
+ float floatType;
+ printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+ printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+ printf("Size of a long int: %zu byte(s)\n", sizeof(lintType));
+ printf("Size of a long long int: %zu byte(s)\n", sizeof(LintType));
+ printf("size of a float: %zu byte(s)\n", sizeof(floatType));
  return (0);
- 
-}
-
+ }
