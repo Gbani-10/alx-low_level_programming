a7221550b45ce8c01409c574aba5b10733064a4c
Gbani-10 <gbani160@gmail.com>
2021-06-17 12:32:12 -0700

    size of computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-/**
+ /**
  * main: print the string in the put function
  *Description: using the main function
  * this program prints "the size of the various type of competer sizes
  *return: 0
  */
 
-int main(void)
-{
-  char c;
-  int i;
-  long 1i;
-  long long 11i;
-  float f;
+ int main(void)
+ {
+ char c;
+ int i;
+ long 1i;
+ long long 11i;
+ float f;
 
-  printf("Size of a char: 1%d byte(s)\n", sizeof(c));
-  printf("Size of an int: 1%d byte(s)\n", sizeof(i));
-  printf("Size of a long int: 1%d byte(s)\n", sizeof(1i));
-  printf("Size of a long long int: 1%d byte(s)\n", sizeof(11i));
-  printf("size of a float: 1%d byte(s)\n", sizeof(f));
+ printf("Size of a char: 1%d byte(s)\n", sizeof(c));
+ printf("Size of an int: 1%d byte(s)\n", sizeof(i));
+ printf("Size of a long int: 1%d byte(s)\n", sizeof(1i));
+ printf("Size of a long long int: 1%d byte(s)\n", sizeof(11i));
+ printf("size of a float: 1%d byte(s)\n", sizeof(f));
 
-  return (0);
+ return (0);
+ 
 }
+
