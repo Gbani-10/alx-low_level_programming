078ad5f5613d4bb29443437edb49cc8d143e4122
Gbani-10 <gbani160@gmail.com>
2021-06-17 12:28:31 -0700

    print same words

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
+ #include <stdio.h>
  /**
  * main- print string using printf 
  * return: 0
  */
 
  int main(void)
-{
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-
-
-}
+ {
+ printf ("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+ }
  
