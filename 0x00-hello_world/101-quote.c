45e7f119a36d2f97d72214314ab0ef54873b0769
Gbani-10 <gbani160@gmail.com>
2021-06-17 11:43:13 -0700

    entry point

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd,h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+  write(2, "and that piece of art is useful\n" - Dora Korpar, 2105-10-19", 59);
+
+return (1);
+
+}
