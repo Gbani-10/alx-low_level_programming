991251d9eb964ab1f4ccc4c2e590f7c444f23866
Gbani-10 <gbani160@gmail.com>
2021-06-21 12:00:22 -0700

    print variables of dirrent combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - print all combinations from 00 01 to 98 99 where the first comb
+ * is less than the second comb
  *
- *Return: Always 0 (Success)
+ * Return: 0
  */
+
 int main(void)
 {
-         int i;
-         int j;
-         for (i=0; i<100; i++)
-         {
-         for (j=i+1; j<100; j++)
-	 {
-         putchar (i/10 + '0');
-	 putchar (i % 10 + '0');
-         putchar ('');
-	 putchar (j / 10 + '0');
-	 putchar (j % 10 + '0');
-	 if (i==98 && j==99)
-	 {
-         break;
-         }
-         putchar (',');
-         putchar ('');
-         putchar ('\n');
+int i, j;
+
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar(i / 10 + 48);
+putchar(i % 10 + 48);
+putchar(32);
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
+if (i < 98)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
 return (0);
 }
