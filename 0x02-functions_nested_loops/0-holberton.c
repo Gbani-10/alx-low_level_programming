e04416766ac31e7c390b04f5b94e7ea9682e88a8
Gbani-10 <gbani160@gmail.com>
2021-06-22 13:28:59 -0700

    print Holberton and a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int _putchar(char c);
+/**
+*main - entry point
+*
+*return: Always 0 (success)
+*/
+main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# All functions and nested lops in C pragramming are practiced in this project
