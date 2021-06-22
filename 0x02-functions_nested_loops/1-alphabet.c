6c89517d1203fc438b945b5d97679c9c6cf02d0c
Gbani-10 <gbani160@gmail.com>
2021-06-22 15:22:34 -0700

    print alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *main - entry point
+ *
+ *Return: always 0 (success)
+*/
+void print_alphbet()
+{
+  char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
