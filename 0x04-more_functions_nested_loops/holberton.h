3aa99244096deb394352447d6a5858624705557d
Gbani-10 <gbani160@gmail.com>
2021-06-24 11:00:28 -0700

    print numbers from 0 to 9

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,6 +9,5 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-#include <stdio.h>
 void print_triangle(int size);
 void print_number(int n);
