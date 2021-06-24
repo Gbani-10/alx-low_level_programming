fcf736456d3020b13b1807490c836005b7c1cee1
Gbani-10 <gbani160@gmail.com>
2021-06-24 12:42:38 -0700

    print largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,57 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <math.h>
 /**
- * main -  program that finds and prints the largest prime factor of a number
- * Return: Always 0.
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	long int num = 612852475143;
-	int prime;
-
-	for (prime = 2; prime <= sqrt(num); prime++)
+	long int n;
+	long int max;
+	long int i;
+	n = 612852475143;
+	max = -1;
+	while (n % 2 == 0)
 	{
-		/*int saved_prime;*/
-
-		if (num % prime == 0)
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
 		{
-			/*saved_prime = prime;*/
-			num = num / prime;
-			prime = 1;
-		/* printf("%ld\n", num);*/
+			max = i;
+			n = n / i;
 		}
 	}
-	printf("%ld\n", num);
-return (0);
+	if (n > 2)
+		max = n;
+	printf("%ld\n", max);
+	return (0);
+}
+12:15
+Task #12 solution
+12:15
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
