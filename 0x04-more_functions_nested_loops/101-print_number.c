0a7c581e4dc9bd3ce2fbcd14e725811240a2fe01
Gbani-10 <gbani160@gmail.com>
2021-06-24 08:18:42 -0700

    print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_number - funtion to print integer
+ *@n: integer for the parameter
+ *
+ *Return: always 0 (success)
+ */
+void print_number(int n)
+{
+   if (n < 0)
+     {
+       putchar('-');
+       n = -n;
+   }
+
+   if (n == 0)
+     {
+      putchar('0');
+     }
+   if (n/10)
+     {
+      printnumber(n/10);
+     }
+   putchar(n%10 + '0');
+}
