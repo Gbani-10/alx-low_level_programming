4860425bafb0e3510c9cf96ce00522ba4b932106
Gbani-10 <gbani160@gmail.com>
2021-06-24 12:39:02 -0700

    print straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
 /**
- * print_line - function that checks for uppercase character.
- * @n: is the integer for the paramaters of my function
- * Return: 0
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-	int a = 0;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		while (a <= n)
+		_putchar('\n');
+	} else
+	{
+		int i;
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
-			a++;
 		}
-	 _putchar('\n');
+		_putchar('\n');
 	}
-	else
-	_putchar('\n');
 }
