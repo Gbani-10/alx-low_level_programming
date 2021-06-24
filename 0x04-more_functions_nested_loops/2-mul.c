b74fdd97d61705d4591d2b9dbc86ef21d3939292
Gbani-10 <gbani160@gmail.com>
2021-06-24 03:58:03 -0700

    print multiples of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.c"
+/**
+ *mul - function that multiplies two integers
+ *@a: integer for the parameter
+ *@b: integer for tne parameter
+ *Return: Always 0
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
