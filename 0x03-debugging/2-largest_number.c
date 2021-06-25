f116a64b464621d6e78a064d06f1a8d445d6cbc2
Gbani-10 <gbani160@gmail.com>
2021-06-25 13:26:41 -0700

    print largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number: returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -15,13 +15,15 @@ if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b >= b && b >= c)
+else if (b >= a && b >= c)
 {
+
 largest = b;
 
 }
 else
 {
+
 largest = c;
 
 }
